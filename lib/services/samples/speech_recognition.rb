34098e0967ceb94c9c94d6714137292941fdeeef
David Stancu <dstancu@nyu.edu>
2017-03-22 20:01:27 -0400

    preliminary implementation needs test update
    [#140718855]

--- lib/services/samples/speech_recognition.rb
+++ lib/services/samples/speech_recognition.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require 'pocketsphinx-ruby'
+require 'streamio-ffmeg'
 
 module Services
   module Samples
@@ -11,20 +12,38 @@ module Services
       end
 
       def compute_hypothesis(sample)
-        recognizer.decode(fetch_buffer(sample.s3_url))
-        recognizer.hypothesis
+        prepare_buffer(sample.s3_url)
+        recognizer.decode(wav_tmpfile)
+        recognizer.hypothesis.tap do
+          File.delete(wav_tmpfile)
+          @tf_path = nil
+        end
       end
 
       private
 
-      def fetch_buffer(s3_url)
-        Adapters::S3.file_to_buffer(s3_url)
+      attr_reader :tf_path
+
+      def prepare_buffer(s3_url)
+        # Dump the buffer to a temporary file
+        tempfile = Tempfile.new('sphinxtemp')
+        tempfile.write(Adapters::S3.file_to_buffer(s3_url))
+        @tf_path = tempfile.path
+
+        # ffmpeg it into PocketSphinx specs
+        ff = FFMPEG::Movie.new(@tf_path)
+        ff.transcode("#{tf_path}-wv", %w(-ar 1600 -ac 1)
+        tempfile.unlink
       end
 
       def recognizer
         @recognizer ||= ::Pocketsphinx::Decoder.new(
           ::Pocketsphinx::Configuration.default
         )
+      end
+
+      def wav_tmpfile
+        "#{@tf_path}-wv"
       end
     end
   end
