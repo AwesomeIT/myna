50785dff664d76ce91a848c276a6f43073701891
David Stancu <dstancu@nyu.edu>
2017-04-23 12:16:02 -0400

    attempt to do heroku deps properly
    update kagu
    
    remove pry
    
    bump version of kagu
    
    specify ruby version
    
    add aptfile
    
    change deps
    
    specify deb for ffmpeg
    
    add build essential
    
    add swig
    
    add more deps
    
    pa
    
    add sphinxbase
    
    use sb3
    
    newer?
    
    use yaketty pkg
    
    cedar16
    
    cableswig

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 # encoding: utf-8
 source 'https://rubygems.org'
 
+ruby '2.4.0'
+
 gem 'activerecord'
 gem 'activesupport'
 
--- app/responders/database_events.rb
+++ app/responders/database_events.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-require 'pry'
 module Responders
   class DatabaseEvents < Base
     # Topics in
