76ca9a7406f5f6b5ce3611c37fc310fc83aa1dcd
David Stancu <dstancu@nyu.edu>
2017-05-13 22:01:31 -0400

    this is why we write tests

--- app/controllers/sample/delete_from_s3.rb
+++ app/controllers/sample/delete_from_s3.rb
@@ -2,8 +2,6 @@
 module Controllers
   module Sample
     class DeleteFromS3 < Base
-      ensure_message
-
       def perform_async
         object = Kagu::Adapters::S3.object_by_key(message[:s3_key])
         return unless object.present?
